147d9daefb1f2e09219149e6ef0f2870b7974f42
Jay H.J Kwon <nicekhj@gmail.com>
2014-02-24 12:18:17 +0900

    'spider'

--- routes/index.js
+++ routes/index.js
@@ -1,15 +1,14 @@
 exports.index = function(req, res){
   res.render('index', { 
     keywords: 'seo, sample, spa', 
-    description: 'show the way to solve seo issue in single page application',
-    title: 'Express' 
+    description: 'show the way to solve seo issue in single page application'
   });
 };
 
 exports.indexSpider = function(req, res){
-  res.render('index_spider', { 
+  res.render('spider/index', { 
+    title: 'seo sample',
     keywords: 'seo, sample, spa', 
-    description: 'show the way to solve seo issue in single page application',
-    title: 'Express' 
+    description: 'show the way to solve seo issue in single page application'
   });
 };
